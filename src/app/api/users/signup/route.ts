cd0ed1e62597cc649709c873e0b4f9f6ff9edcaf
Ankit Ydv <ankityadav11241@gmail.com>
2023-11-13 23:57:14 +0530

    add: signup API

---
+++ src/app/api/users/signup/route.ts
@@ -0,0 +1,43 @@
+import { connect } from '@/dbConfig/dbConfig';
+import User from '@/models/userModel';
+import { NextRequest, NextResponse } from 'next/server';
+import bcryptjs from 'bcryptjs';
+
+
+connect();
+
+export async function POST(request: NextRequest) {
+    try {
+        const reqBody = await request.json();
+        const {email, password, username} = reqBody;
+
+        console.log(reqBody);
+        // check if user already exists
+        const user = await User.findOne({email});
+        if (user) {
+            return NextResponse.json({status: 400, error: "User already exists"});
+        }
+
+        // hash password
+        const salt = await bcryptjs.genSalt(10);
+        const hashedPassword = await bcryptjs.hash(password, salt);
+
+        const newUser = new User({
+            email,
+            password: hashedPassword,
+            username
+        });
+
+        const savedUser = await newUser.save();
+        console.log(savedUser);
+
+        return NextResponse.json({
+            message: "User created successfully",
+            success: true,
+            savedUser,
+        });
+
+    } catch (error: any) {
+        return NextResponse.json({ error: error.message }, { status: 500 });
+    }
+}

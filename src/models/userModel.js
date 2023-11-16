709247535a7610ce884e9679ebbd08f9c098e386
Ankit Ydv <ankityadav11241@gmail.com>
2023-11-16 12:33:23 +0530

    Whitespace removed

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -26,7 +26,7 @@ const userSchema = new mongoose.Schema({
     forgotPasswordToken: String,
     forgotPasswordTokenExpiry: Date,
     verifyToken: String,
-    verifyTokenExpiry: Date,  
+    verifyTokenExpiry: Date,
 })
 
 const User = mongoose.models.users || mongoose.model('users', userSchema);
